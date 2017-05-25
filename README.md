a080c2ec230427db2b91262509d175f95d3e3f5f
Dan Brooks <dmbrooks@live.co.uk>
2017-05-25 13:31:07 +0100

    Add catamorphism

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Scala recursion schemes
 
 Recursion schemes explained with Scala.
+
+## Catamorphism
+
+    List(1, 2, 4, 8).fold(0)(_ + _) should be(15)
--- src/test/scala/recursion_schemes/CataSpec.scala
+++ src/test/scala/recursion_schemes/CataSpec.scala
@@ -2,6 +2,8 @@ package recursion_schemes
 
 import org.scalatest._
 
-class CataSpec extends FlatSpec with Matchers {
-
+class CataSpec extends FreeSpec with Matchers {
+  "simple catamorphism" in {
+    List(1, 2, 4, 8).fold(0)(_ + _) should be(15)
+  }
 }
