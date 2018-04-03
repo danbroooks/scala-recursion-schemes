f5721647c18e23de5aa92a7c1b3d9d3baf55a1ae
Dan Brooks <dmbrooks@live.co.uk>
2018-04-03 12:19:06 +0100

    -

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ scalaVersion := "2.11.7"
 
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
-
+libraryDependencies += "com.slamdata" %% "matryoshka-core" % "0.18.3"
--- src/test/scala/recursion_schemes/CataSpec.scala
+++ src/test/scala/recursion_schemes/CataSpec.scala
@@ -1,9 +1,42 @@
 package recursion_schemes
 
 import org.scalatest._
+import matryoshka._
+import matryoshka.implicits._
+import matryoshka.data.Mu
+
+sealed abstract class Expr[A]
+final case class Num[A](value: Long) extends Expr[A]
+final case class Mul[A](l: A, r: A)  extends Expr[A]
 
 class CataSpec extends FreeSpec with Matchers {
   "simple catamorphism" in {
     List(1, 2, 4, 8).fold(0)(_ + _) should be(15)
+  }
+
+  "" in {
+    implicit val exprFunctor = new scalaz.Functor[Expr] {
+      override def map[A, B](fa: Expr[A])(f: (A) => B) = fa match{
+        case Num(value) => Num[B](value)
+        case Mul(l, r) => Mul(f(l), f(r))
+      }
+    }
+
+    val eval: Algebra[Expr, Long] = {
+      case Num(x)    => x
+      case Mul(x, y) => x + y
+    }
+
+    val uneval: Coalgebra[Expr, Long] = {
+      case x => Num(x)
+    }
+
+    def someExpr[T](implicit T: Corecursive.Aux[T, Expr]): T =
+      Mul(Num[T](2).embed, Mul(Num[T](3).embed,
+          Num[T](4).embed).embed).embed
+
+    // someExpr[Mu[Expr]].ana(9) should be(9)
+
+    9L.ana.apply(uneval) should be(9L)
   }
 }
